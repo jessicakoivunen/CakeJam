21855a8512bfc47583becb36fa4b3740102adfd5
Jessica Koivunen <fjeko015@edu.xamk.fi>
2024-02-16 23:50:43 +0200

    movement + animation started

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     //Movement
-    public float speed = 10f;
+    public float speed = 1.0f;
+    public Animator animator;
+    private Vector2 movement;
+    private Rigidbody2D rb;
 
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
