8e0b8ba55db635e323df66d739d1491281042b67
Mikko Anttila <fmian007@edu.xamk.fi>
2024-02-17 14:55:27 +0200

    Changed knife to fish for testing visuals, inventory slots have visuals, scaling of inventory slots is fixed and dropping things is easier, dropped things now spawn on the player but cant be picked up until you move away and toward them again (to prevent player accidentally dropping things on walls.)

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,24 @@ public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
     public GameObject itemButton;
+    private float pickupDelay = 1f;
 
     private void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
+    private void Update()
+    {
+        if (pickupDelay > 0)
+        {
+            pickupDelay -= Time.deltaTime;
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player") && pickupDelay <= 0) {
             for (int i = 0; i < inventory.slots.Length; i++) {
                 if (inventory.isFull[i] == false)
                 {
