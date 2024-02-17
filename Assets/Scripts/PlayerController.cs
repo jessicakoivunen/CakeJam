7d9c6633e3543092e50ee37c5e2dbf9fbc69e9e2
Jessica Koivunen <fjeko015@edu.xamk.fi>
2024-02-17 10:19:52 +0200

    movement working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,27 +13,26 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnMovement(InputValue value)
     {
+        movement = value.Get<Vector2>();
 
+        if (movement.x != 0 || movement.y != 0) { 
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("IsWalking", true);
+        }
+        else {
+            animator.SetBool("IsWalking", false);
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Movement
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-        transform.position = transform.position + new Vector3(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime, 0);
-
-
+        rb.MovePosition(rb.position +  movement * speed * Time.fixedDeltaTime);
     }
 }
