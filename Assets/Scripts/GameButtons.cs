fb02dcb7004459b2412acb8d9f3b71ed3728e84d
Mikko Anttila <fmian007@edu.xamk.fi>
2024-02-18 10:43:34 +0200

    Pause menu added

--- Assets/Scripts/GameButtons.cs
+++ Assets/Scripts/GameButtons.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,28 @@ using UnityEngine.SceneManagement;
 
 public class GameButtons : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public PlayerController grandma;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeInHierarchy)
+        {
+            pauseMenu.SetActive(false);
+            grandma.paused = false;
+        } else if (Input.GetKeyDown(KeyCode.Escape) && !pauseMenu.activeInHierarchy && grandma.paused == false)
+        {
+            pauseMenu.SetActive(true);
+            grandma.paused = true;
+        }
+    }
+
     public void ExitGame()
     {
-        Debug.Log("Quit button clicked");
-        Application.Quit();
+        SceneManager.LoadScene("MainMenu");
     }
     public void Restart()
     {
-        SceneManager.LoadScene("Game");    // main scene
+        SceneManager.LoadScene("Game");
     }
 }
