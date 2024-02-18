e4053d167fa3bdc8dfea1d64874af1d58ab55f21
Mikko Anttila <fmian007@edu.xamk.fi>
2024-02-18 16:13:58 +0200

    Added a "warning" for when youre able to bake a cake.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,9 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+
+    public GameObject warning;
+
+    private void Update()
+    {
+        if(isFull[0] && isFull[1] && isFull[2])
+        {
+            warning.SetActive(true);
+        } else if(warning.activeInHierarchy)
+        {
+            warning.SetActive(false);
+        }
+    }
 }
